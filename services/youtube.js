c06ba60c7535c0769783c2749a0591126ff32e2d
SylvainWinning <168933918+SylvainWinning@users.noreply.github.com>
2024-12-12 14:29:41 +0100

    youtube.js

--- services/youtube.js
+++ services/youtube.js
@@ -1,5 +1,5 @@
 import { google } from 'googleapis';
-import { youtubeAuth } from '../auth/oauth2Client.js';
+import { youtubeAuth } from './auth/oauth2Client.js';
 
 export const youtube = google.youtube({
   version: 'v3',
