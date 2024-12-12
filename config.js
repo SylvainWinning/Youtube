fa4f583af5156944b623467fc5fdbd0d38993524
SylvainWinning <168933918+SylvainWinning@users.noreply.github.com>
2024-12-12 11:41:51 +0100

    config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+export const config = {
+  youtubeApiKey: "AIzaSyAwCedx65-TQaSdUnC0NDMzZWf3S4HjDWg",
+  oauth2ClientId: "370667794508-sm2m0t9blehb3rindukpka3vuoh7kquc.apps.googleusercontent.com",
+  oauth2ClientSecret: "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",
+  redirectUri: "http://localhost:3000/oauth2callback",
+  youtubeTokens: {
+    "access_token": "ya29.a0ARW5m74TLW6z0GAXyE1u0xQPzynlrr74LUJx9cOVFtBPauiu9bZ83eZxxiLy9ncCWMgWAVZ0nGelnmZaVdRmkIAKWvJKi6FwMU4g2kXOo6BngO2Zc_lhIO6jhYgmrdLI7Hy1RBxN4j2nVtXvKvvrqLC9O0zKbAxswlqfxVStaCgYKAZUSARESFQHGX2MipY1W8Voys1WkLhrgMt8XeA0175",
+    "scope": "https://www.googleapis.com/auth/youtube.readonly",
+    "token_type": "Bearer",
+    "expires_in": 3599,
+    "refresh_token": "1//04l9Lx9_w0G9WCgYIARAAGAQSNwF-L9IrOHzAH8unn1BshAOdVMY-CSWbeA-4MuR8webqSc3PK5_PGUkTxBBuUcNivJjEhziQAgY"
+  },
+  sheetsTokens: {
+    "access_token": "ya29.a0AeDClZCelK0-R6zU2ds24pmPmKvPOcVCNHBTTxJEa-zo6DtBDisAaTiGPf7yXptablWR-5z98I5JJWdRnhy8eKx0tH95PyTu5ozC3OdAteM4BAtEUK_6ktgjiotUY4N6jygB4wK7MVkgWCNoSMvnsIT0yqT41pX_JAIMa3XiaCgYKAawSARESFQHGX2MiCmW6H09TB1A1BCIK8Va_nw0175",
+    "scope": "https://www.googleapis.com/auth/spreadsheets",
+    "token_type": "Bearer",
+    "expires_in": 3599,
+    "refresh_token": "1//04KbTYGbV9TR6CgYIARAAGAQSNwF-L9Ir76S-cRGaBxO7YJBBCxKhYrkAxtSMd4lynt45ObIlIDfwuJ-KZzdmH4g1uWNePRzhsWs"
+  }
+}
