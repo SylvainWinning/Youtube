b04ad36032a264fc64149d921db8abc6ad90d0aa
SylvainWinning <168933918+SylvainWinning@users.noreply.github.com>
2024-12-15 19:47:32 +0100

    config.js

--- config.js
+++ config.js
@@ -1,20 +1,27 @@
 export const config = {
-  youtubeApiKey: "AIzaSyAwCedx65-TQaSdUnC0NDMzZWf3S4HjDWg",
-  oauth2ClientId: "370667794508-sm2m0t9blehb3rindukpka3vuoh7kquc.apps.googleusercontent.com",
-  oauth2ClientSecret: "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",
-  redirectUri: "http://localhost:3000/oauth2callback",
-  youtubeTokens: {
-    "access_token": "ya29.a0AeDClZDKscJQ1i3hMvYV7ekC7EuqRaEUTNJJx3pzhJR6iY4RY7NSYt4NuVvfPg9eQNBub60N03F6oCCtVyyzoqh32briNXmyy3O3P39fLqJzxWcXBKj32lKEDaY9EnhQd5ZoqUCneAUVwxJwhrZzRl6hTaTFyl9CUUl8wdhHaCgYKAV8SARESFQHGX2MiTXiHFnb8h4GtvpCvkGtn8A0175", 
-    "scope": "https://www.googleapis.com/auth/youtube.readonly", 
-    "token_type": "Bearer", 
-    "expires_in": 3599, 
-    "refresh_token": "1//04xz5aqAAf_0LCgYIARAAGAQSNwF-L9IrsAzT17HeHcYyXSHttYr5UM25MbeQMgEUDPqAdQ9yFkDlNV8Icn2znekM96VxOJ3kJSo"
+  google: {
+    clientId: "370667794508-sm2m0t9blehb3rindukpka3vuoh7kquc.apps.googleusercontent.com",
+    clientSecret: "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",
+    redirectUri: "http://localhost:3000/oauth2callback",
+    refreshToken: "1//04xz5aqAAf_0LCgYIARAAGAQSNwF-L9IrsAzT17HeHcYyXSHttYr5UM25MbeQMgEUDPqAdQ9yFkDlNV8Icn2znekM96VxOJ3kJSo",
   },
-  sheetsTokens: {
-    "access_token": "ya29.a0ARW5m74LlveXZi3k0pMpdJ35WbGbSc2fp7Bgq9gGyFWnspYhNt7T9uj6J4fPCLiKhxk2miZ4P-4PphrZq-a-yFcSyhKdATqvNvXu6J8Jxt-G1MjoUNoh3jTZLbABviwKzeF6ONBiZoEY7G0aI_N9uVwUHXZJCDMAAWqtLPwsaCgYKAesSARESFQHGX2MiFHcUI4qkTFvN-oeFJzD97w0175", 
-    "scope": "https://www.googleapis.com/auth/spreadsheets", 
-    "token_type": "Bearer", 
-    "expires_in": 3599, 
-    "refresh_token": "1//04PNKcmppuEmTCgYIARAAGAQSNwF-L9IrP-V5Q3FcGuFYkHArE73KCLuqomFYUTyava0U7YRlewPsqSFkH5Su5TRWuwxd_GKt1f4"
-  }
-}
+  youtube: {
+    apiKey: "AIzaSyAwCedx65-TQaSdUnC0NDMzZWf3S4HjDWg",
+    tokens: {
+      accessToken: "ya29.a0AeDClZDKscJQ1i3hMvYV7ekC7EuqRaEUTNJJx3pzhJR6iY4RY7NSYt4NuVvfPg9eQNBub60N03F6oCCtVyyzoqh32briNXmyy3O3P39fLqJzxWcXBKj32lKEDaY9EnhQd5ZoqUCneAUVwxJwhrZzRl6hTaTFyl9CUUl8wdhHaCgYKAV8SARESFQHGX2MiTXiHFnb8h4GtvpCvkGtn8A0175",
+      refreshToken: "1//04xz5aqAAf_0LCgYIARAAGAQSNwF-L9IrsAzT17HeHcYyXSHttYr5UM25MbeQMgEUDPqAdQ9yFkDlNV8Icn2znekM96VxOJ3kJSo",
+      scope: "https://www.googleapis.com/auth/youtube.readonly",
+      tokenType: "Bearer",
+      expiresIn: 3599,
+    },
+  },
+  sheets: {
+    tokens: {
+      accessToken: "ya29.a0ARW5m74LlveXZi3k0pMpdJ35WbGbSc2fp7Bgq9gGyFWnspYhNt7T9uj6J4fPCLiKhxk2miZ4P-4PphrZq-a-yFcSyhKdATqvNvXu6J8Jxt-G1MjoUNoh3jTZLbABviwKzeF6ONBiZoEY7G0aI_N9uVwUHXZJCDMAAWqtLPwsaCgYKAesSARESFQHGX2MiFHcUI4qkTFvN-oeFJzD97w0175",
+      refreshToken: "1//04PNKcmppuEmTCgYIARAAGAQSNwF-L9IrP-V5Q3FcGuFYkHArE73KCLuqomFYUTyava0U7YRlewPsqSFkH5Su5TRWuwxd_GKt1f4",
+      scope: "https://www.googleapis.com/auth/spreadsheets",
+      tokenType: "Bearer",
+      expiresIn: 3599,
+    },
+  },
+};
