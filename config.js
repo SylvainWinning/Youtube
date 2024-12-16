4d7aefba13b9a52e05009dd8d34476fadca09338
SylvainWinning <168933918+SylvainWinning@users.noreply.github.com>
2024-12-16 10:50:34 +0100

    config.js

--- config.js
+++ config.js
@@ -1,27 +1,35 @@
+import dotenv from 'dotenv';
+
+// Charger les variables d'environnement depuis le fichier `.env`
+dotenv.config();
+
 export const config = {
   google: {
-    clientId: "370667794508-sm2m0t9blehb3rindukpka3vuoh7kquc.apps.googleusercontent.com",
-    clientSecret: "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",
-    redirectUri: "http://localhost:3000/oauth2callback",
-    refreshToken: "1//04xz5aqAAf_0LCgYIARAAGAQSNwF-L9IrsAzT17HeHcYyXSHttYr5UM25MbeQMgEUDPqAdQ9yFkDlNV8Icn2znekM96VxOJ3kJSo",
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    redirectUri: process.env.GOOGLE_REDIRECT_URI,
+    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
   },
   youtube: {
-    apiKey: "AIzaSyAwCedx65-TQaSdUnC0NDMzZWf3S4HjDWg",
+    apiKey: process.env.YOUTUBE_API_KEY,
     tokens: {
-      accessToken: "ya29.a0AeDClZDKscJQ1i3hMvYV7ekC7EuqRaEUTNJJx3pzhJR6iY4RY7NSYt4NuVvfPg9eQNBub60N03F6oCCtVyyzoqh32briNXmyy3O3P39fLqJzxWcXBKj32lKEDaY9EnhQd5ZoqUCneAUVwxJwhrZzRl6hTaTFyl9CUUl8wdhHaCgYKAV8SARESFQHGX2MiTXiHFnb8h4GtvpCvkGtn8A0175",
-      refreshToken: "1//04xz5aqAAf_0LCgYIARAAGAQSNwF-L9IrsAzT17HeHcYyXSHttYr5UM25MbeQMgEUDPqAdQ9yFkDlNV8Icn2znekM96VxOJ3kJSo",
-      scope: "https://www.googleapis.com/auth/youtube.readonly",
-      tokenType: "Bearer",
-      expiresIn: 3599,
+      accessToken: process.env.YOUTUBE_ACCESS_TOKEN, // Facultatif si besoin
+      refreshToken: process.env.YOUTUBE_REFRESH_TOKEN, // Facultatif si besoin
+      scope: 'https://www.googleapis.com/auth/youtube.readonly',
+      tokenType: 'Bearer', // Gardez ceci par défaut
+      expiresIn: process.env.YOUTUBE_TOKEN_EXPIRES_IN || 3599, // Facultatif
     },
   },
   sheets: {
     tokens: {
-      accessToken: "ya29.a0ARW5m74LlveXZi3k0pMpdJ35WbGbSc2fp7Bgq9gGyFWnspYhNt7T9uj6J4fPCLiKhxk2miZ4P-4PphrZq-a-yFcSyhKdATqvNvXu6J8Jxt-G1MjoUNoh3jTZLbABviwKzeF6ONBiZoEY7G0aI_N9uVwUHXZJCDMAAWqtLPwsaCgYKAesSARESFQHGX2MiFHcUI4qkTFvN-oeFJzD97w0175",
-      refreshToken: "1//04PNKcmppuEmTCgYIARAAGAQSNwF-L9IrP-V5Q3FcGuFYkHArE73KCLuqomFYUTyava0U7YRlewPsqSFkH5Su5TRWuwxd_GKt1f4",
-      scope: "https://www.googleapis.com/auth/spreadsheets",
-      tokenType: "Bearer",
-      expiresIn: 3599,
+      accessToken: process.env.SHEETS_ACCESS_TOKEN, // Facultatif si besoin
+      refreshToken: process.env.SHEETS_REFRESH_TOKEN, // Facultatif si besoin
+      scope: 'https://www.googleapis.com/auth/spreadsheets',
+      tokenType: 'Bearer', // Gardez ceci par défaut
+      expiresIn: process.env.SHEETS_TOKEN_EXPIRES_IN || 3599, // Facultatif
     },
+  },
+  app: {
+    logLevel: process.env.LOG_LEVEL || 'INFO', // Niveau de log par défaut
   },
 };
